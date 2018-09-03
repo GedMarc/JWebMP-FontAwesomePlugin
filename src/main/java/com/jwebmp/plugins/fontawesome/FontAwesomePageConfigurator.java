9bd880aaa12b65d23449d41d3007e7246baf1fb0
GedMarc <ged_marc@hotmail.com>
2018-09-03 22:50:50 +0200

    Make page configuration optional, update the app

--- src/main/java/com/jwebmp/plugins/fontawesome/FontAwesomePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/fontawesome/FontAwesomePageConfigurator.java
@@ -45,10 +45,39 @@ import javax.validation.constraints.NotNull;
 public class FontAwesomePageConfigurator
 		implements IPageConfigurator
 {
+	/**
+	 * If this configurator is enabled
+	 */
+	private static boolean enabled = true;
 
 	public FontAwesomePageConfigurator()
 	{
 		//Nothing Needed
+	}
+
+	/**
+	 * Method isEnabled returns the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @return the enabled (type boolean) of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static boolean isEnabled()
+	{
+		return FontAwesomePageConfigurator.enabled;
+	}
+
+	/**
+	 * Method setEnabled sets the enabled of this AngularAnimatedChangePageConfigurator object.
+	 * <p>
+	 * If this configurator is enabled
+	 *
+	 * @param mustEnable
+	 * 		the enabled of this AngularAnimatedChangePageConfigurator object.
+	 */
+	public static void setEnabled(boolean mustEnable)
+	{
+		FontAwesomePageConfigurator.enabled = mustEnable;
 	}
 
 	@NotNull
@@ -63,5 +92,11 @@ public class FontAwesomePageConfigurator
 			    .add(FontAwesomeReferencePool.FontAwesomeReference.getCssReference());
 		}
 		return page;
+	}
+
+	@Override
+	public boolean enabled()
+	{
+		return FontAwesomePageConfigurator.enabled;
 	}
 }
