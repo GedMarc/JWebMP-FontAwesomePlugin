e9f70b41cf617f458e4f797a3035d8e70456921b
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:00 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,5 +1,8 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.fontawesome.FontAwesomePageConfigurator;
+import com.jwebmp.plugins.fontawesome.implementations.FontAwesomeExclusionsModule;
 
 module com.jwebmp.plugins.fontawesome {
 	exports com.jwebmp.plugins.fontawesome;
@@ -10,7 +13,12 @@ module com.jwebmp.plugins.fontawesome {
 
 	requires java.validation;
 	requires java.logging;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with FontAwesomePageConfigurator;
+
+	provides IGuiceScanJarExclusions with FontAwesomeExclusionsModule;
+	provides IGuiceScanModuleExclusions with FontAwesomeExclusionsModule;
+
 	opens com.jwebmp.plugins.fontawesome to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
